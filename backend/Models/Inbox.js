f946cd2c7e7c484dd10f5d6e1a018b3e570e6364
josh <josueh6758@gmail.com>
2018-11-25 20:27:13 -0500

    added inbox model

---
+++ backend/Models/Inbox.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const InboxSchema = new Schema({
+	subject: {
+		type: String,
+		required: true,
+		max: 20
+	},
+	message: {
+		type: String,
+		required: true
+		min: 10,
+		max: 150
+	},
+	recipient: {
+		type: Schema.Types.ObjectId,
+		ref:'UserModel'
+		required: true 
+	},
+	sender: {
+		type: Schema.Types.ObjectId,
+		ref:'UserModel'
+		required: true 
+	},
+	sent: {
+		type: Date,
+		default: Date.now 
+	},
+	viewed:{
+		type: Boolean,
+		required: true,
+		default: false
+	}
+
+});
+
+module.exports = mongoose.model('Inbox', InboxSchema);
