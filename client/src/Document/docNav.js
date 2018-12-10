fc5cdd9317f0b0bea35d7b367bad3289a3736a06
Carlos Saravia <crsaravia00@gmail.com>
2018-12-09 22:43:00 -0500

    Added button and icons for DocNav

--- client/src/Document/docNav.js
+++ client/src/Document/docNav.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';  
+import {Navbar, NavbarBrand, Nav, NavItem, Button} from 'reactstrap';  
 
 class DocNav extends Component {
 	render() {
 		return (
-			<div className = "navDoc">
+			<div className="navHome">
 				<Navbar light expand="md">
-				<NavbarBrand href="/">BHS</NavbarBrand>
+					<NavbarBrand href="/">BHS</NavbarBrand>
+					<Nav className= "ml-auto" navbar>
+						<NavItem>
+							<i className = "material-icons docicons" >people</i>
+						</NavItem>
+						<NavItem>
+							<i className = "material-icons docicons">history</i>
+						</NavItem>
+						<NavItem>
+							<Button color = "info" style = {{marginTop:"5px"}}>Save</Button>
+						</NavItem>
+					</Nav>
 				</Navbar>
 			</div>
 		);
